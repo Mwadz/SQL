dfbea358f8eb9f1ce327a059fd5c4352eac207d1
Mwadz <mghoimwadime@gmail.com>
2024-06-11 04:46:01 -0700

    correlation

--- CovidDataCleaning.sql
+++ CovidDataCleaning.sql
@@ -1,5 +1,24 @@
 
 use project1;
 
+/* Data Preview */
+
 select *
 from nashvillehousing;
+
+
+
+/* 1.
+ Normalizing date format */
+
+SELECT SaleDate
+FROM project1.nashvillehousing;
+
+
+SELECT  STR_TO_DATE(SaleDate, '%m/%d/%Y')
+FROM    yourtable
+
+/* confirming datatype conversion */
+
+SELECT DATA_TYPE FROM INFORMATION_SCHEMA.COLUMNS 
+  WHERE table_name = 'nashvillehousing' AND COLUMN_NAME = 'SaleDate';
